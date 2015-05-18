4e134f24359161ca35bec6a2d2d0bbf03558e241
Togam <margot_six@hotmail.com>
2015-05-18 16:43:05 +0200

    ChargerDepuisBase => case Commande
    Cas des commande fonctionnel dans ChargerDepuisBase
    Test Unitaires MAJ et fonctionnels

--- PersistanceSQL.java
+++ PersistanceSQL.java
@@ -17,19 +17,6 @@ public class PersistanceSQL {
 	     this.ipBase = ipBase ;
 	     this.port = port ; 
 	     this.nomBaseDonne = nomBaseDonne;
-	     try  {
-	    	 Class.forName("org.gjt.mm.mysql.Driver");
-	    	   
-	    	 connexion = DriverManager.getConnection("jdbc:mysql://" + ipBase + ":" +port + "/" + nomBaseDonne ,"root","");
-	        	
-	     }
-	     catch(SQLException e) {
-	        System.out.println("SQL error : " + e.getMessage());
-	     }
-	     catch(ClassNotFoundException e){
-	        System.out.println("Class not found : " + e.getMessage());
-	     }
-	     
 	 }
 	
 	   public void RangerDansBase(){
@@ -47,206 +34,202 @@ public class PersistanceSQL {
 	
 	    public Object ChargerDepuisBase(String id , String nomClasse){
 	    	Object objet = new Object();
-	    	
-	    	try {
-	    		Statement stmt = connexion.createStatement();
 
 		    	switch(nomClasse){
-			        case "Produit" :     	
-			        	try
-			        	{
+			        case "Produit" : 
+				        try  {
+				        	Class.forName("org.gjt.mm.mysql.Driver");
+				   	    	connexion = DriverManager.getConnection("jdbc:mysql://" + ipBase + ":" +port + "/" + nomBaseDonne ,"root","");  	
+				   	     }
+				   	     catch(SQLException e) {
+				   	        System.out.println("SQL error : " + e.getMessage());
+				   	     }
+				   	     catch(ClassNotFoundException e){
+				   	        System.out.println("Class not found : " + e.getMessage());
+				   	     }
+				        	
+				        	try
+				        	{
+				        
+				        		Statement stmt1 = connexion.createStatement();
+					            String query = "SELECT V.Libelle, L.Type, C.taille "
+					            		+ "FROM variete V, livraison L, lotproduction P, verger N, calibre C "
+					            		+ "WHERE P.NumLot = \""+id+"\" "
+					            		+ "AND P.CodeLiv = L.CodeLiv "
+					            		+ "AND P.Calibre = C.IdCalibre "
+					            		+ "AND L.CodeVerger = N.CodeVerger "
+					            		+ "AND N.LibelleVar = V.Libelle";
+					            ResultSet rs = stmt1.executeQuery(query);
+					            
+					            String variete = "";
+					            String typeProduit = "";
+					            int calibre = 0;
+					            
+					            while(rs.next())
+					            {
+					            	variete = rs.getString("V.Libelle");
+					            	typeProduit = rs.getString("L.Type");
+					            	calibre = rs.getInt("C.taille");
+					            }
+					            	
+					            objet = new Produit(variete,typeProduit,calibre);
+					            
+					        }
+					        catch(SQLException e)
+					        {
+					            System.out.println("SQL error : " + e.getMessage());
+					        }
+				        	catch(java.lang.NullPointerException e){
+				        		System.out.println("NullPointerException error : " + e.getMessage());
+				        	}
+				        	finally {
+				        		try
+					            {
+				        			if(connexion != null) {
+				       	             connexion.close();
+				        			}
+					            }
+					            catch(SQLException e)
+					            {
+					                System.out.println("SQL error cannot close bdd " + e.getMessage());
+					            }
+					            catch(java.lang.NullPointerException e)
+					            {
+					                System.out.println("SQL error cannot close bdd " + e.getMessage());
+					            }
+				        	}
+			        break;
+			        
+			        case "Commande" :	
+				        try  {
+				   	    	 Class.forName("org.gjt.mm.mysql.Driver");  
+				   	    	 connexion = DriverManager.getConnection("jdbc:mysql://" + ipBase + ":" +port + "/" + nomBaseDonne ,"root","");	
+				   	     }
+				   	     catch(SQLException e) {
+				   	        System.out.println("SQL error : " + e.getMessage());
+				   	     }
+				   	     catch(ClassNotFoundException e){
+				   	        System.out.println("Class not found : " + e.getMessage());
+				   	     }
+				        	
+				        	try
+				        	{
+				        		Statement stmt2 = connexion.createStatement();
+					            String query2 = "SELECT E.Libelle, D.Quantite, C.Date, E.Date, C.prixHT "
+					            		+ "FROM commande C, comporter D, conditionnement E "
+					            		+ "WHERE C.CodeCom = \""+id+"\" "
+					            		+ "AND C.CodeCom = D.NumCom "
+					            		+ "AND E.IdCond = D.IdCond";
+					            ResultSet rs2 = stmt2.executeQuery(query2);
+					            
+					            String conditionnement="";
+					            int qte = 0;
+					            Date dateE = null;
+					            Date dateC = null;
+					            float prix = 0;
+					            
+					            while(rs2.next())
+					            {
+					            	conditionnement = rs2.getString("E.Libelle");
+					            	qte = rs2.getInt("D.Quantite");
+					            	dateE = rs2.getDate("C.Date");
+					            	dateC = rs2.getDate("E.Date");
+					            	prix = rs2.getFloat("C.prixHT");
+					            }
+					            
+					            Statement stmt3 = connexion.createStatement();
+					            String query3 = "SELECT NumLot "
+					            		+ "FROM commande "
+					            		+ "WHERE CodeCom = \""+id+"\" ";
+					            ResultSet rs3 = stmt3.executeQuery(query3);
+					            
+					            int numProd = 0;
+					            while(rs3.next())
+					            {
+					            	numProd = rs3.getInt("NumLot");
+					            }
+					            id = Integer.toString(numProd);
 				            
-				            String query = "SELECT V.Libelle, L.Type, C.taille "
-				            		+ "FROM variete V, livraison L, lotproduction P, verger N, calibre C "
-				            		+ "WHERE P.NumLot = \""+id+"\" "
-				            		+ "AND P.CodeLiv = L.CodeLiv "
-				            		+ "AND P.Calibre = C.IdCalibre "
-				            		+ "AND L.CodeVerger = N.CodeVerger "
-				            		+ "AND N.LibelleVar = V.Libelle";
-				            ResultSet rs = stmt.executeQuery(query);
-				            
-				            String variete = "";
-				            String typeProduit = "";
-				            int calibre = 0;
-				            
-				            while(rs.next())
-				            {
-				            	variete = rs.getString("V.Libelle");
-				            	typeProduit = rs.getString("L.Type");
-				            	calibre = rs.getInt("C.taille");
-				            }
-				            	
-				            objet = new Produit(variete,typeProduit,calibre);
-				            
-				        }
-				        catch(SQLException e)
-				        {
-				            System.out.println("SQL error : " + e.getMessage());
-				        }
-			        	catch(java.lang.NullPointerException e){
-			        		System.out.println("NullPointerException error : " + e.getMessage());
-			        	}
-			        case "Commande" :
-			        	try
-			        	{
-			        		//RAJOUTER E.Date
-				            String query = "SELECT E.Libelle, D.Quantite, C.Date "
-				            		+ "FROM commande C, comporter D, conditionnement E "
-				            		+ "WHERE C.CodeCom =\""+id+"\" "
-				            		+ "AND C.CodeCom = D.NumCom "
-				            		+ "AND E.IdCond = D.IdCond";
-				            ResultSet rs = stmt.executeQuery(query);
-				            
-				            String conditionnement="";
-				            int qte = 0;
-				            Date dateE = null;
-				            Date dateC = null;
-				            
-				            while(rs.next())
-				            {
-				            	conditionnement = rs.getString("E.Libelle");
-				            	qte = rs.getInt("E.Quantite");
-				            	dateE = rs.getDate("C.Date");
-				            	//dateC = rs.getDate("E.Date");
-				            }
-				            
-				            
-				            
-				            String query2 = "SELECT NumLot FROM commande";
-				            ResultSet rs2 = stmt.executeQuery(query2);
-				            int numProd = rs2.getInt("NumLot");
-				            id = Integer.toString(numProd);
-				            Object LeProd = new Object();
-				            LeProd = this.ChargerDepuisBase(id, "Produit");
-				            
-				            //A COMPLETER ?
-				            float prix = 0;
-				            
-				            objet = new Commande(Integer.parseInt(id), ((Produit)LeProd), prix, conditionnement, qte, dateE, dateC);
-				            
-			        	}
-		        		 catch(SQLException e)
-				        {
-				            System.out.println("SQL error : " + e.getMessage());
-				        }
-			        	catch(java.lang.NullPointerException e){
-			        		System.out.println("NullPointerException error : " + e.getMessage());
-			        	}
-		    	}
-	    	}
-	    	catch(SQLException e)
-		    {
-	    		System.out.println("SQL error : " + e.getMessage());
-		    }
-	        catch(java.lang.NullPointerException e){
-	        	System.out.println("NullPointerException error : " + e.getMessage());
-	        }
-	    	
-	    	try
-            {
-                connexion.close();
-            }
-            catch(SQLException e)
-            {
-                System.out.println("SQL error cannot close bdd " + e.getMessage());
-            }
-            catch(java.lang.NullPointerException e)
-            {
-                System.out.println("SQL error cannot close bdd " + e.getMessage());
-            }
-	    	
-	        /*switch(nomClasse){
-		        case "Produit" :     	
-		        	try
-		        	{
-			            Statement stmt = connexion.createStatement();
-			            String query = "SELECT V.Libelle, L.Type, C.taille "
-			            		+ "FROM variete V, livraison L, lotproduction P, verger N, calibre C "
-			            		+ "WHERE P.NumLot = \""+id+"\" "
-			            		+ "AND P.CodeLiv = L.CodeLiv "
-			            		+ "AND P.Calibre = C.IdCalibre "
-			            		+ "AND L.CodeVerger = N.CodeVerger "
-			            		+ "AND N.LibelleVar = V.Libelle";
-			            ResultSet rs = stmt.executeQuery(query);
-			            
-			            String variete = "";
-			            String typeProduit = "";
-			            int calibre = 0;
-			            
-			            while(rs.next())
-			            {
-			            	variete = rs.getString("V.Libelle");
-			            	typeProduit = rs.getString("L.Type");
-			            	calibre = rs.getInt("C.taille");
-			            }
-			            	
-			            objet = new Produit(variete,typeProduit,calibre);
-			            
-			        }
-			        catch(SQLException e)
-			        {
-			            System.out.println("SQL error : " + e.getMessage());
-			        }
-		        	catch(java.lang.NullPointerException e){
-		        		System.out.println("NullPointerException error : " + e.getMessage());
-		        	}
-		        	finally
-			        {
-			            try
-			            {
-			                connexion.close();
-			            }
-			            catch(SQLException e)
-			            {
-			                System.out.println("SQL error cannot close bdd " + e.getMessage());
-			            }
-			            catch(java.lang.NullPointerException e)
-			            {
-			                System.out.println("SQL error cannot close bdd " + e.getMessage());
-			            }
-			               
-			        }
-		        case "Commande" :
-		        	try
-		        	{
-			            Statement stmt = connexion.createStatement();
-			            
-			            String query = "SELECT CodeCom FROM commande";
-			            ResultSet rs = stmt.executeQuery(query);
-			            String idCom = rs.getString("CodeCom");
-			            
-			            String query2 = "SELECT NumLot FROM commande";
-			            ResultSet rs2 = stmt.executeQuery(query2);
-			            int numProd = rs2.getInt("NumLot");
-			            id = Integer.toString(numProd);
-			            Object LeProd = new Object();
-			            
-			           // LeProd = ps.ChargerDepuisBase(id, "Produit");
-		        	}
-	        		 catch(SQLException e)
-			        {
-			            System.out.println("SQL error : " + e.getMessage());
-			        }
-		        	catch(java.lang.NullPointerException e){
-		        		System.out.println("NullPointerException error : " + e.getMessage());
-		        	}
-			        finally
-			        {
-			            try
-			            {
-			                connexion.close();
-			            }
-			            catch(SQLException e)
-			            {
-			                System.out.println("SQL error cannot close bdd " + e.getMessage());
-			            }
-			            catch(java.lang.NullPointerException e)
-			            {
-			                System.out.println("SQL error cannot close bdd " + e.getMessage());
-			            }
-			               
-			        }
-	        	/*case "Distributeur" :
+					            Produit LeProd = new Produit("0","0",0);
+					            
+					            try
+					        	{
+					        
+					        		Statement stmt4 = connexion.createStatement();
+						            String query = "SELECT V.Libelle, L.Type, C.taille "
+						            		+ "FROM variete V, livraison L, lotproduction P, verger N, calibre C "
+						            		+ "WHERE P.NumLot = \""+numProd+"\" "
+						            		+ "AND P.CodeLiv = L.CodeLiv "
+						            		+ "AND P.Calibre = C.IdCalibre "
+						            		+ "AND L.CodeVerger = N.CodeVerger "
+						            		+ "AND N.LibelleVar = V.Libelle";
+						            ResultSet rs4 = stmt4.executeQuery(query);
+						            
+						            String variete = "";
+						            String typeProduit = "";
+						            int calibre = 0;
+						            
+						            while(rs4.next())
+						            {
+						            	variete = rs4.getString("V.Libelle");
+						            	typeProduit = rs4.getString("L.Type");
+						            	calibre = rs4.getInt("C.taille");
+						            }
+						            	
+						            LeProd = new Produit(variete,typeProduit,calibre);
+						            
+						        }
+						        catch(SQLException e)
+						        {
+						            System.out.println("SQL error : " + e.getMessage());
+						        }
+					        	catch(java.lang.NullPointerException e){
+					        		System.out.println("NullPointerException error : " + e.getMessage());
+					        	} 
+					            
+	
+					            objet = new Commande(Integer.parseInt(id), LeProd, prix, conditionnement, qte, dateE, dateC);
+					            
+				        	}
+			        		 catch(SQLException e)
+					        {
+					            System.out.println("SQL error : " + e.getMessage());
+					        }
+				        	catch(java.lang.NullPointerException e){
+				        		System.out.println("NullPointerException error : " + e.getMessage());
+				        	}
+				        	finally {
+				        		try
+					            {
+				        			if(connexion != null) {
+				       	             connexion.close();
+				        			}
+					            }
+					            catch(SQLException e)
+					            {
+					                System.out.println("SQL error cannot close bdd " + e.getMessage());
+					            }
+					            catch(java.lang.NullPointerException e)
+					            {
+					                System.out.println("SQL error cannot close bdd " + e.getMessage());
+					            }
+				        	}
+			        	break;
+		    	
+
+	        	case "Distributeur" :
+	        		try  {
+			        	Class.forName("org.gjt.mm.mysql.Driver");
+			   	    	connexion = DriverManager.getConnection("jdbc:mysql://" + ipBase + ":" +port + "/" + nomBaseDonne ,"root","");  	
+			   	     }
+			   	     catch(SQLException e) {
+			   	        System.out.println("SQL error : " + e.getMessage());
+			   	     }
+			   	     catch(ClassNotFoundException e){
+			   	        System.out.println("Class not found : " + e.getMessage());
+			   	     }
+	        		
+	        		
 	        		try
 		        	{
 			            Statement stmt = connexion.createStatement();
@@ -265,7 +248,9 @@ public class PersistanceSQL {
 			        {
 			            try
 			            {
-			                connexion.close();
+			            	if(connexion != null) {
+			       	            connexion.close();
+			        		}
 			            }
 			            catch(SQLException e)
 			            {
@@ -277,7 +262,8 @@ public class PersistanceSQL {
 			            }
 			               
 			        }
-	        }	*/
+	        		break;
+	        }	
 	        
 	    	
 			return objet;
--- PersistanceSQLTest.java
+++ PersistanceSQLTest.java
@@ -21,35 +21,46 @@ public class PersistanceSQLTest {
 	}
 
 	@Test
-	public void testChargerDepuisBase__Produit() {
-			PersistanceSQL ps = new PersistanceSQL("localhost", 3306,"vdev");
+	public void testChargerDepuisBase() throws ParseException {
+			PersistanceSQL ps = new PersistanceSQL("localhost", 3306,"vdev2");
 			
 			Object objet = new Produit("var1","fraiche entiere",10);
 			Object objet2= ps.ChargerDepuisBase("1", "Produit");
-
 			
 			assertEquals(objet.getClass(),objet2.getClass());
 			assertEquals(((Produit) objet).getVariete(),((Produit) objet2).getVariete());
 			assertEquals(((Produit) objet).getType(),((Produit) objet2).getType());
 			assertEquals(((Produit) objet).getCalibre(),((Produit) objet2).getCalibre());
+			
+			SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+			Date dE = format.parse("01-01-2015");
+			Date dC = format.parse("01-01-2015");
+			
+			Object objet3 = new Commande(1, (Produit)objet, 10.F, "cond1",10, dE,dC);
+			Object objet4 = ps.ChargerDepuisBase("1", "Commande");
+			
+			
+			assertNotEquals(objet3.getClass(),objet.getClass());
+			assertEquals(objet3.getClass(),objet4.getClass());
+			
+			assertEquals(((Commande)objet3).getId(),((Commande)objet4).getId());
+			
+			Produit prod1 = ((Commande) objet3).getProduit();
+			Produit prod2 = ((Commande) objet4).getProduit();
+			
+			assertEquals(prod1.getVariete(),prod2.getVariete());
+			assertEquals(prod1.getType(),prod2.getType());
+			assertEquals(prod1.getCalibre(),prod2.getCalibre());
+			
+			assertEquals(((Commande)objet3).getConditionnement(),((Commande)objet4).getConditionnement());
+			assertEquals(((Commande)objet3).getPrixHT(),((Commande)objet4).getPrixHT(),0.1);
+			assertEquals(((Commande)objet3).getQte(),((Commande)objet4).getQte());
+			assertEquals(((Commande)objet3).getDateEnvoi(), ((Commande)objet4).getDateEnvoi());
+			assertEquals(((Commande)objet3).getDateConditionnement(), ((Commande)objet4).getDateConditionnement());
+			
+			
 		
 	}
-	
-	public void testChargerDepuisBase__Commande() throws ParseException {
-		PersistanceSQL ps = new PersistanceSQL("localhost", 3306,"vdev");
-		
-		Object objet = new Produit("var1","fraiche entiere",10);
-		
-		
-		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
-		Date dE = format.parse("01-01-2015");
-		Date dC = format.parse("01-01-2015");
-		
-		Object objet3 = new Commande(1, (Produit)objet, 0.F, "cond1",10, dE,dC);
-		Object objet4 = ps.ChargerDepuisBase("1", "Commande");
-		
-		assertEquals(objet3.getClass(),objet4.getClass());
-		assertEquals(((Commande) objet3).getProduit(),((Commande) objet3).getProduit());
-}
+
 
 }
